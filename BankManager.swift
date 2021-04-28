13b3f07bb0ce85adfc8bdd1ebe453b85df711d2c
sjsj <boromir21@naver.com>
2021-04-28 20:37:51 +0900

    [feat] Step1 BankManager 타입 구현 및 콘솔앱 연습

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,34 @@
 //
 
 import Foundation
+
+class BankManager {
+    let queue = OperationQueue()
+    var banker: Int
+    var customer: Int
+    
+    init(banker: Int) {
+        self.banker = banker
+    }
+    
+    func openBank() {
+        generationCustomer()
+        print("은행 개점")
+        workOfBanker()
+    }
+    
+    func closeBank() {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer)명이며, 총 업무시간은 \(Double(customer) * 0.7)초입니다.")
+    }
+    
+    func generationCustomer() {
+        let customerCount = Int.random(in: 10...30)
+        customer = customerCount
+    }
+    
+    func workOfBanker() {
+        for _ in 0...customer {
+            
+        }
+    }
+}
